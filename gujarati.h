451b3f8316f22ceb340fda56a3650dd2527718bc
Soham Dalwadi <64000094+sohamdalwadi@users.noreply.github.com>
2023-06-10 12:55:25 +0530

    Create gujarati.h

---
+++ gujarati.h
@@ -0,0 +1,32 @@
+#ifndef GUJARATI_H
+#define GUJARATI_H
+
+// Data Types
+#define અક char
+#define સંખ્યા int
+#define પરમ long
+#define ડબલ double
+#define અનસાઇનેડ unsigned
+#define અકશર char
+#define શબ્દ string
+
+// Control Flow
+#define જો if
+#define નહીં_તો else
+#define કરો do
+#define જ્યાસુધી while
+
+// Functions
+#define પ્રોટોટાઇપ prototype
+#define પાછું return
+#define નામબંધન namespace
+#define કાર્ય function
+#define લંબવતોનેવજવું inline
+#define સ્થિર const
+#define આંતરરસગ્રહીત volatile
+#define બાહ્ય extern
+
+// Entry Point
+#define મુખ્ય main
+
+#endif // GUJARATI_H
